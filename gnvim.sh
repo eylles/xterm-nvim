31ab8797916060a33deaa7d85f6fde5e95f62075
eylles <ed.ylles1997@gmail.com>
2025-03-29 22:42:40 -0600

    Add scripts

---
+++ gnvim.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+term_cmd="x-terminal-emulator"
+
+config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/gnvim"
+config_file="${config_dir}/configrc"
+
+# loading the config here means the user can overwrite any of the functions
+if [ -f "$config_file" ]; then
+    . "$config_file"
+else
+    if [ ! -d "$config_dir" ]; then
+        mkdir -p "$config_dir"
+    fi
+    cat << __HEREDOC__ >> "$config_file"
+# vim: ft=sh
+# gnvim wrapper config file
+
+# terminal command
+term_cmd="x-terminal-emulator"
+
+# neovim command
+nv_cmd="/usr/bin/nvim"
+__HEREDOC__
+fi
+
+myname="${0##*/}"
+
+# cuz $UID is not POSIX ¯\_(ツ)_/¯
+# but it may be defined in the environment
+if [ -z "$UID" ]; then
+  # result of: id -u $USER
+  UserID=$(id -u "$USER")
+else
+  UserID="$UID"
+fi
+
+run_dir="/var/run/user/${UserID}/gnvim"
+run_file="${run_dir}/gnvim.lock"
+
+# unix command line like booleans
+
+# Type: int
+# value: 0
+b_true=0
+# Type: int
+# value: 1
+b_false=1
+
+cleanup() {
+    rm "$run_file"
+}
+
+trap cleanup EXIT
+
+if [ -f "$run_file" ]; then
+    hasterm="$b_true"
+    date +"[%F %T]: ${myname} called." >> "$run_file"
+else
+    hasterm="$b_false"
+    if [ ! -d "$run_dir" ]; then
+        mkdir -p "$run_dir"
+    fi
+    date +"[%F %T]: ${myname} called." > "$run_file"
+fi
+
+if [ ! -f "$run_file" ]; then
+    exit 1
+else
+    if [ "$hasterm" -eq "$b_true" ]; then
+        exec tmux-nvim "$@"
+    else
+        $term_cmd -e tmux-nvim "$@"
+    fi
+    cleanup
+fi
---
+++ tmux-nvim.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# wrapper to launch neovim inside tmux
+
+nv_cmd="/usr/bin/nvim"
+
+config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/gnvim"
+config_file="${config_dir}/configrc"
+
+# loading the config here means the user can overwrite any of the functions
+if [ -f "$config_file" ]; then
+    . "$config_file"
+else
+    if [ ! -d "$config_dir" ]; then
+        mkdir -p "$config_dir"
+    fi
+    cat << __HEREDOC__ >> "$config_file"
+# vim: ft=sh
+# gnvim wrapper config file
+
+# terminal command
+term_cmd="x-terminal-emulator"
+
+# neovim command
+nv_cmd="${nv_cmd}"
+__HEREDOC__
+fi
+
+if [ -n "${1}" ]; then
+    vimcmd="$nv_cmd ${1}"
+else
+    vimcmd="$nv_cmd"
+fi
+
+if [ -z "$TMUX" ] ;then
+    ID=$(tmux ls | grep nvim | cut -d: -f1) # get the id of a detached session
+    if [ -z "$ID" ] ;then
+        tmux new-session -s "nvim" "${vimcmd}" \; attach # if not existing, create one
+        # tmux attach-session -t "nvim" # attach to existing
+    else
+        tmux attach-session -t "$ID" # attach to existing
+        tmux new-window "${vimcmd}" # if existing, open new tab
+        # tmux next-window "$ID"
+    fi
+fi
