8aa81bc09b5edc22ded12abe4362ecc149e5d9ba
eylles <ed.ylles1997@gmail.com>
2025-05-09 16:05:59 -0600

    Add wayland support

--- vim-anywhere.sh
+++ vim-anywhere.sh
@@ -7,10 +7,32 @@ chmod 600 "$TMP_FILE"
 # start vim in insert mode
 vimopts="+star"
 
+clipboard_to_file () {
+    case "$XDG_SESSION_TYPE" in
+        x11)
+            xclip -o -selection clipboard > "$TMP_FILE"
+            ;;
+        wayland)
+            wl-paste > "$TMP_FILE"
+            ;;
+    esac
+}
+
+type_file () {
+    case "$XDG_SESSION_TYPE" in
+        x11)
+            xdotool type "$(cat $TMP_FILE)"
+            ;;
+        wayland)
+            wtype "$(cat $TMP_FILE)"
+            ;;
+    esac
+}
+
 case "$1" in
     -c|clipboard)
         shift
-        xclip -o -selection clipboard > "$TMP_FILE"
+        clipboard_to_file
         ;;
 esac
 
@@ -20,6 +42,6 @@ gnvim "--class" "GVim" "$vimopts" "$TMP_FILE"
 
 # yeh some delay for your window to recover focus...
 sleep 0.6
-xdotool type "$(cat $TMP_FILE)"
+type_file
 
 rm "$TMP_FILE"
