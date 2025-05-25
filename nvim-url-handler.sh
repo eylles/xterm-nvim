dbc4e303438fd2f5974763c0e4e196832b16feeb
eylles <ed.ylles1997@gmail.com>
2025-05-24 18:00:40 -0600

    Broaden schema support
    We can support schemas like:
        vim://open?file=/path/to/file&line=123
        mvim://open/?url=file:///path/to/file&line=123
        nvim://open?uri=file:///path/to/file&line=123
        gvim://open/?url=file:///path/to/file&line=123
        gnvim://open/?file=/path/to/file&line=123

--- nvim-url-handler.sh
+++ nvim-url-handler.sh
@@ -10,21 +10,44 @@ ___HEREDOC
 }
 
 # Parse the nvim:// URL (e.g. nvim://open?file=/path/to/file&line=123)
+
+# URI can come in one of the following forms:
+#
+# With only a '?' preceding the keyword
+#     vim://open?file=/path/to/file&line=123
+#     vim://open?url=file:///path/to/file&line=123
+#     vim://open?uri=file:///path/to/file&line=123
+#
+# With '/?' preceding the keyword
+#     vim://open/?file=/path/to/file&line=123
+#     vim://open/?url=file:///path/to/file&line=123
+#     vim://open/?uri=file:///path/to/file&line=123
 uri="$1"
 case "$uri" in
     *line=*)
         line="${uri##*&line=}"
         ;;
     *)
-        line="${uri##nvim://open?file=*}"
-        line="${uri##mvim://open?file=*}"
-        line="${uri##vim://open?file=*}"
+        line="${uri##*vim://open?file=*}"
+        line="${uri##*vim://open?url=*}"
+        line="${uri##*vim://open?uri=*}"
+
+        line="${uri##*vim://open/?file=*}"
+        line="${uri##*vim://open/?url=*}"
+        line="${uri##*vim://open/?uri=*}"
         ;;
 esac
 line="${line:-1}"
-uri="${uri##nvim://open?file=}"
-uri="${uri##mvim://open?file=}"
-uri="${uri##vim://open?file=}"
+
+uri="${uri##*vim://open?file=}"
+uri="${uri##*vim://open?url=}"
+uri="${uri##*vim://open?uri=}"
+
+uri="${uri##*vim://open/?file=}"
+uri="${uri##*vim://open/?url=}"
+uri="${uri##*vim://open/?uri=}"
+
+
 uri="${uri%%&line=*}"
 file=$(uridecode "${uri}")
 
